ed8a000aa9124bda913b484be02bb2849a7fb689
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-04 01:29:44 +0900

    feat: 로그인 입력 값 상태 업데이트

--- petharmony/src/login/components/LoginModal.jsx
+++ petharmony/src/login/components/LoginModal.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/LoginModal.css";
 import logo from "../../common/logo/assets/logo.png";
 import LoginInput from "./LoginInput";
-import LoginButton from "./LoginButton";
+import LoginButton from "../../common/button/components/LoginJoinButton";
 
 const LoginModal = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log(
+            "Email: ", email,
+            "Password: ", password
+        );
+    };
+
     return (
         <>
             <div className="login_modal">
                 <img className="lm_logo" src={logo} alt="로고" />
-                <form className="lm_form">
-                    <LoginInput />
+                <div className="lm_form">
+                    <LoginInput
+                        setEmail={setEmail}
+                        setPassword={setPassword}
+                    />
                     <div className="lm_self">
                         <LoginButton
                             mode="login"
+                            type="submit"
+                            onClick={handleLogin}
                         />
                         <LoginButton
-                            mode="join"
+                            mode="goJoin"
                         />
                     </div>
                     <div className="lm_social">
@@ -27,7 +45,7 @@ const LoginModal = () => {
                             mode="google"
                         />
                     </div>
-                </form>
+                </div>
             </div>
         </>
     )
