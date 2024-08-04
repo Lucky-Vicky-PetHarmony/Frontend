bf22ffb35e6dc6ebfb16757a7e019721a81e2711
Gaannini <0107162a@gmail.com>
2024-08-04 23:46:28 +0900

    feat: 게시판 카테고리 선택버튼

---
+++ petharmony/src/board/component/BoardSelectBtn.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import '../style/BoardSelectBtn.css'
+
+const BoardSelectBtn = () => {
+    const [activeBtn, setActiveBtn] = useState("all");
+
+    const handleButtonClick = (buttonName) => {
+        setActiveBtn(buttonName);
+    };
+
+    return (
+        <div className="board_select_btn_group">
+            <button 
+                className={`board_select_btn ${activeBtn === 'all' ? 'active all' : ''}`}
+                onClick={() => handleButtonClick("all")}>전체</button>
+            <button 
+                className={`board_select_btn ${activeBtn === 'free' ? 'active free' : ''}`}
+                onClick={() => handleButtonClick("free")}>자유</button>
+            <button 
+                className={`board_select_btn ${activeBtn === 'adoption' ? 'active adoption' : ''}`}
+                onClick={() => handleButtonClick("adoption")}>입양</button>
+            <button 
+                className={`board_select_btn ${activeBtn === 'information' ? 'active information' : ''}`}
+                onClick={() => handleButtonClick("information")}>정보</button>
+        </div>
+    );
+}
+
+export default BoardSelectBtn;
