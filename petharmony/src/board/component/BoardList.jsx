8d50cdada79eb6084e1d77afb92ee277329dc4b7
Gaannini <0107162a@gmail.com>
2024-08-05 02:13:28 +0900

    feat: 게시판 배너, 검색창, 필터 통합

--- petharmony/src/board/component/BoardList.jsx
+++ petharmony/src/board/component/BoardList.jsx
@@ -4,15 +4,23 @@ import BoardContent from "./BoardContent";
 import BoardPagination from "./BoardPagination";
 import BoardSelectBtn from "./BoardSelectBtn";
 import BoardWriteBtn from "./BoardWriteBtn";
+import boardbannerimg from "../asset/boardbanner.png"
+import BoardSearch from "./BoardSearch";
+import BoardFilter from "./BoardFilter";
 
 
 const BoardList = () => {
 
     return (
         <div className="boardlist">
-            <div className="boardlist_top">
+            <img src={boardbannerimg} alt="" />
+            <div className="boardlist_top_top">
                 <BoardSelectBtn/>
                 <BoardWriteBtn/>
+            </div>
+            <div className="boardlist_top_bottom">
+                <BoardFilter/>
+                <BoardSearch/>
             </div>
             <div className="boardlist_middle">
                 <BoardContent/>
