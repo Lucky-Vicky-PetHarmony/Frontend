2e65b0039e0a0793e09ce70e34368f87f03c5679
Gaannini <0107162a@gmail.com>
2024-08-05 02:12:49 +0900

    feat: 게시판 검색창

---
+++ petharmony/src/board/component/BoardSearch.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import '../style/BoardSearch.css';
+import searchImg from "../asset/search.png";
+
+const BoardSearch = () => {
+    return (
+        <div className="board_search">
+            <input type="text" placeholder="검색어를 입력하세요"/>
+            <img src={searchImg} alt="" />
+        </div>
+    );
+}
+
+export default BoardSearch;
