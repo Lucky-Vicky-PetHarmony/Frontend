57331a897c3acffefe3c77d375e299b1c05853b8
Gaannini <0107162a@gmail.com>
2024-08-08 22:27:36 +0900

    feat: 게시글등록/카테고리선택

---
+++ petharmony/src/board/component/BoardPost/BoardPostCategory.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../../style/boardPost/BoardPostCategory.css';
+import BoardSelectBtn from '../boardList/BoardSelectBtn';
+
+
+const BoardPostCategory = () => {
+
+    return (
+        <div className="BPCategory">
+            <p>글 작성에 앞서 글 내용에 맞는 카테고리를 선택해주세요.
+                <span> * 미선택시 자유게시판에 게시됩니다.</span>
+            </p>
+            <BoardSelectBtn  post={"free"}/>
+        </div>
+    );
+}
+
+export default BoardPostCategory;
