1b64672c838a1a7300974b888fee30a5948d9ae8
Gaannini <0107162a@gmail.com>
2024-08-08 22:54:23 +0900

    chore: navigator 등록페이지 이동

--- petharmony/src/board/component/boardList/BoardWriteBtn.jsx
+++ petharmony/src/board/component/boardList/BoardWriteBtn.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import '../../style/boardList/BoardWriteBtn.css';
+import { useNavigate } from 'react-router-dom';
 
 const BoardWriteBtn = () => {
+    const nav = useNavigate();
 
     return (
-        <button className="board_write_btn">글쓰기</button>  
+        <button 
+            className="board_write_btn" 
+            onClick={() => nav('/boardPost')}>글쓰기</button>  
     );
 }
 
