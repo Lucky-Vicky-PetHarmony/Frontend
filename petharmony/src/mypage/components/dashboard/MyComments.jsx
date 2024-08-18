f84a2d4a53319d3f303d1c79bceb385d3c99c727
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-19 04:19:23 +0900

    feat: 마이페이지 > 내가 쓴 댓글, 내가 쓴 게시물, PIN 게시물 컴포넌트 생성

---
+++ petharmony/src/mypage/components/dashboard/MyComments.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../../common.css";
+import "../../styles/dashboard/MyComments.css";
+
+const MyComments = () => {
+    return (
+        <div className="my_comments">
+            <p className="mc_title">내가 쓴 댓글</p>
+        </div>
+    )
+}
+
+export default MyComments;
---
+++ petharmony/src/mypage/components/dashboard/PinPosts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../../common.css";
+import "../../styles/dashboard/PinPosts.css";
+
+const PinPosts = () => {
+    return (
+        <div className="pin_posts">
+            <p className="pp_title">PIN 게시물</p>
+        </div>
+    )
+}
+
+export default PinPosts;
