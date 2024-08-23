51ebfd998583758a8324dcb375fe906316dd89a0
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-23 18:38:55 +0900

    style: 정렬

--- petharmony/src/mypage/components/dashboard/PinPosts.jsx
+++ petharmony/src/mypage/components/dashboard/PinPosts.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../../styles/dashboard/PinPosts.css";
 import BoardListElem from "../../../board/component/boardList/BoardListElem";
-import axios from "axios";
 
 const PinPosts = ({ token }) => {
+    // PIN 게시물 목록 상태
     const [pinPosts, setPinPosts] = useState([]);
 
+    // PIN 게시물 가져오는 함수
     useEffect(() => {
         const fetchPinPosts = async () => {
             try {
@@ -19,16 +21,16 @@ const PinPosts = ({ token }) => {
                 console.error("핀 게시물을 가져오는 데 실패했습니다:", error);
             }
         };
-
         fetchPinPosts();
     }, [token]);
 
     return (
         <div className="pin_posts">
             <p className="pp_title">PIN 게시물</p>
-            <div className="pin_posts_list">
+            <div className="pp_content">
                 {pinPosts.map(post => (
-                    <BoardListElem key={post.boardId} board={post} />
+                    // customClass prop을 전달하여 특정 CSS 스타일을 적용할 수 있게 함
+                    <BoardListElem key={post.boardId} board={post} customClass="custom_board_list_elem" />
                 ))}
             </div>
         </div>
