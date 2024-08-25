1bc9d0ab1ecf42c847cf342d0bde5d40a9755dd9
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-25 21:52:43 +0900

    feat: Swiper 라이브러리 사용하여 슬라이드 쇼

---
+++ petharmony/src/main/components/slide/SlideItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/slide/SlideItem.css";
+
+const SlideItem = ({ popFile, noticeNo, sexCd, age }) => {
+    return (
+        <div className="slide_item">
+            <img className="si_img" src={popFile} alt="유기동물사진"/>
+            <p className="si_location">{noticeNo}</p>
+            <p className="si_sex_age">{sexCd} | {age}살</p>
+        </div>
+    );
+}
+
+export default SlideItem;
---
+++ petharmony/src/main/components/slide/Slider.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import "../../styles/slide/Slider.css";
+import SlideItem from "./SlideItem";
+import axios from "axios";
+import loading from "../../assets/slideLoading.png";
+
+const Slider = () => {
+    // 슬라이더 저장할 상태
+    const [slides, setSlides] = useState([]);
+
+    // 슬라이더 데이터 가져오기
+    useEffect(() => {
+        const fetchSlides = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/public/slides');
+                setSlides(response.data);
+            } catch (error) {
+                console.error("슬라이드를 가져오는 데 실패했습니다:", error);
+            }
+        };
+        fetchSlides();
+    }, []);
+
+
+    return (
+        <div className="slider">
+            <Swiper
+                modules={[Autoplay]}                // Autoplay 모듈 추가
+                spaceBetween={52}                   // 간격 : 52
+                slidesPerView={6}                   // 보여질 슬라이드 개수 : 6
+                loop={true}                         // 슬라이드 반복 설정
+                autoplay={{
+                    delay: 1000,                    // 1초마다 자동으로 슬라이드 전환
+                    disableOnInteraction: false,
+                }}
+                speed={1000}                        // 슬라이드 전환 속도 : 1초
+                direction={'horizontal'}            // 슬라이드 방향 : 수평
+            >
+                {slides?.length > 0 ? (
+                    slides.map((slide, index) => (
+                        <SwiperSlide key={index}>
+                            <SlideItem {...slide} />
+                        </SwiperSlide>
+                    ))
+                ) : (
+                    // 데이터가 없을 경우 || 로딩중일때
+                    <img className="slider_loading" src={loading} alt="로딩화면" />
+                )}
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
