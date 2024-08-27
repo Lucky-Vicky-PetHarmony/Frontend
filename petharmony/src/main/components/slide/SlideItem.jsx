75ec7ec0a0cfdd7fcfa96f33983c9af11899a006
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-27 18:05:59 +0900

    chore: 주석 해제

--- petharmony/src/main/components/slide/SlideItem.jsx
+++ petharmony/src/main/components/slide/SlideItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../../styles/slide/SlideItem.css";
 
-const SlideItem = ({ popFile, orgNm, sexCd, age }) => {
+const SlideItem = ({ popFile, sexCd, age, noticeNo }) => {
     return (
         <div className="slide_item">
             <img className="si_img" src={popFile} alt="유기동물사진"/>
-            <p className="si_location">{orgNm}</p>
+            <p className="si_location">{noticeNo}</p>
             <p className="si_sex_age">{sexCd} | {age}</p>
         </div>
     );
