a3940204c80062b251847c8def67b68112082dd4
CHAERIM LEE <leechaerim214@gmail.com>
2024-09-24 03:04:33 +0900

    refactor: 페이지네이션 추가

--- petharmony/src/mypage/components/dashboard/MyComments.jsx
+++ petharmony/src/mypage/components/dashboard/MyComments.jsx
@@ -2,33 +2,41 @@ import React, { useState, useEffect } from "react";
 import axiosInstance from "../../../api/axiosConfig";
 import "../../styles/dashboard/MyComments.css";
 import MyCommentList from "../dashboard/commentList/MyCommentList";
+import BoardPagination from "../../../board/components/boardList/BoardPagination";
 
 const MyComments = () => {
     const [commentData, setCommentData] = useState([]);
+    const [totalPages, setTotalPages] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
 
     // 내가 쓴 댓글
     useEffect(() => {
         const fetchMyComments = async () => {
             try {
-                const response = await axiosInstance.get('/api/user/myComments');
-                if (response.status === 200) {
-                    setCommentData(response.data);
-                }
+                const response = await axiosInstance.get(`/api/user/myComments?page=${currentPage - 1}&size=5`);
+                setCommentData(response.data.content);
+                setTotalPages(response.data.totalPages);
             } catch (error) {
                 console.error("댓글 데이터를 가져오는 데 실패했습니다.", error);
             }
         };
         fetchMyComments();
-    }, []);
+    }, [currentPage]);
 
     return (
         <div className="my_comments">
             <p className="mc_title">내가 쓴 댓글</p>
             <div className="mc_content">
-                {commentData.map(comment => (
-                    <MyCommentList key={comment.commId} comment={comment} />
+                {commentData.map((comment, index) => (
+                    <MyCommentList key={index} comment={comment} />
                 ))}
             </div>
+            <BoardPagination
+                customClass="custom_board_pagination3"
+                setPage={setCurrentPage}
+                totalPages={totalPages}
+                currentPage={currentPage}
+            />
         </div>
     )
 }
--- petharmony/src/mypage/components/dashboard/MyPosts.jsx
+++ petharmony/src/mypage/components/dashboard/MyPosts.jsx
@@ -2,32 +2,42 @@ import React, { useState, useEffect } from "react";
 import axiosInstance from "../../../api/axiosConfig";
 import "../../styles/dashboard/MyPosts.css";
 import BoardListElem from "../../../board/components/boardList/BoardListElem";
+import BoardPagination from "../../../board/components/boardList/BoardPagination";
 
 const MyPosts = () => {
     const [posts, setPosts] = useState([]);
+    const [totalPages, setTotalPages] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
 
-    // PIN 게시물
+    // 내가 쓴 게시물
     useEffect(() => {
         const fetchMyPosts = async () => {
             try {
-                const response = await axiosInstance.get('/api/user/myPosts');
-                setPosts(response.data);
+                const response = await axiosInstance.get(`/api/user/myPosts?page=${currentPage - 1}&size=10`);
+                setPosts(response.data.content);
+                setTotalPages(response.data.totalPages);
             } catch (error) {
-                console.error("핀 게시물을 가져오는 데 실패했습니다:", error);
+                console.error("내가 쓴 게시물을 가져오는 데 실패했습니다:", error);
             }
         };
         fetchMyPosts();
-    }, []);
+    }, [currentPage]);
 
     return (
         <div className="my_posts">
             <p className="mps_title">내가 쓴 게시물</p>
             <div className="mps_content">
-                {posts.map(post => (
+                {posts.map((post, index) => (
                     // customClass prop을 전달하여 특정 CSS 스타일을 적용할 수 있게 함
-                    <BoardListElem key={post.boardId} board={post} customClass="custom_board_list_elem" />
+                    <BoardListElem key={index} board={post} customClass="custom_board_list_elem" />
                 ))}
             </div>
+            <BoardPagination
+                customClass="custom_board_pagination2"
+                setPage={setCurrentPage}
+                totalPages={totalPages}
+                currentPage={currentPage}
+            />
         </div>
     )
 }
